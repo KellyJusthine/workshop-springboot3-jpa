67a0aa9a2b89710041934acf5421b58e8bf1c503
Kelly Justhine <kellyjusthine2003@gmail.com>
2024-08-15 19:59:38 -0300

    Category

---
+++ demo/src/main/java/com/curso/demo/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.curso.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.curso.demo.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
