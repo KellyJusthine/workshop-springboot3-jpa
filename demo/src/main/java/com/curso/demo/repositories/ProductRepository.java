c9fa1937a9da2155ea0451bceb0540a5b7f9d7b3
Kelly Justhine <kellyjusthine2003@gmail.com>
2024-08-15 20:27:56 -0300

    Product

---
+++ demo/src/main/java/com/curso/demo/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.curso.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.curso.demo.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
