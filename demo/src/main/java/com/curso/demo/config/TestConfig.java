210f1c56e7338c2b41c941802ec8d7be5564193f
Kelly Justhine <kellyjusthine2003@gmail.com>
2024-08-17 16:53:43 -0300

    Product-OrderItem many-to-many association

--- demo/src/main/java/com/curso/demo/config/TestConfig.java
+++ demo/src/main/java/com/curso/demo/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.curso.demo.entities.Category;
 import com.curso.demo.entities.Order;
 import com.curso.demo.entities.OrderItem;
+import com.curso.demo.entities.Payment;
 import com.curso.demo.entities.Product;
 import com.curso.demo.entities.User;
 import com.curso.demo.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner{
 
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 		
+		System.out.println("Application is running");
 	}
 }
